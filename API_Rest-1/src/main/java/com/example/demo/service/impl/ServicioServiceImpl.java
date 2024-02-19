19f8667b54732f7f18d95474d8ace9e6ff673950
SamuraiJuanjo <Juanjosepi.orellana19@gmail.com>
2024-02-19 10:49:18 +0100

    añadido proFam

--- API_Rest-1/src/main/java/com/example/demo/service/impl/ServicioServiceImpl.java
+++ API_Rest-1/src/main/java/com/example/demo/service/impl/ServicioServiceImpl.java
@@ -131,6 +131,10 @@ public class ServicioServiceImpl implements ServicioService {
 					|| servicioModel.getValoration() < 0) {
 				servicio.setValoration(servicioModel.getValoration());
 			}
+			
+			if(servicioModel.getProfesionalFamilyId().getId() != -1) {
+				servicio.setProfesionalFamilyId(servicioModel.getProfesionalFamilyId());
+			}
 
 			servicio = servicioRepository.save(servicio);
 		}
